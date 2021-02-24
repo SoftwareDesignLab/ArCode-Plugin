acf269dbaee3e42bc58e76dd5ae1e6df18cb30cd
a-shokri <alishok62@gmail.com>
2021-02-24 09:33:30 -0500

    Fixing ClassHierarchy building issue with default IntelliJ JRE

--- src/main/java/com/ibm/wala/util/PlatformUtil.java
+++ src/main/java/com/ibm/wala/util/PlatformUtil.java
@@ -14,8 +14,8 @@ import java.util.stream.Collectors;
     This class is a replacement for com.ibm.wala.util.PlatformUtil to make an adhoc fix on getBootClassPathJars() method
  */
 
-public class PlatformUtil {
-    public PlatformUtil() {
+public class PlatformUtil_ {
+    public PlatformUtil_() {
     }
 
     public static boolean onMacOSX() {
@@ -74,7 +74,7 @@ public class PlatformUtil {
                 }
             }
 
-            return (String[])result.toArray(new String[0]);
+            return (String[])result.toArray( new String[result.size()] );
         }
     }
 }
